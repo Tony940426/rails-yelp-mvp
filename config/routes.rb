0a285cb6f4dfeccf424dfdeec42d6d04abdc6ce4
Tony Wang <tony94042@gmail.com>
2022-11-18 12:18:12 +1100

    Applied All CRUD actions for Restaurant and Refactored

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit'
+  patch 'restaurants/:id', to: 'restaurants#update'
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
